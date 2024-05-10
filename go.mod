2a53c6baba078d1efb6dd23484109ca4b4997570
windeesel365 <windeesel365@gmail.com>
2024-05-10 18:01:03 +0700

    feat: add struct schema

---
+++ go.mod
@@ -0,0 +1,3 @@
+module swagger-go-api
+
+go 1.22.3
--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+type Shopper struct {
+	Username   string `json:"username" gorm:"primaryKey"`
+	FullName   string `json:"fullName"`
+	Email      string `json:"email"`
+	Street     string `json:"street"`
+	City       string `json:"city"`
+	State      string `json:"state"`
+	ZipCode    string `json:"zipCode"`
+	DateJoined string `json:"dateJoined"`
+}
+
+type ShoppersResponse struct {
+	Shoppers []Shopper `json:"shoppers"`
+}
+
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
 func main() {
 
 }
