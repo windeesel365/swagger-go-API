9357121ab8a0c47fe798e20eed230e0d45b48a1b
windeesel365 <windeesel365@gmail.com>
2024-05-10 18:03:29 +0700

    feat: add global variable db and initDB

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
 type Shopper struct {
 	Username   string `json:"username" gorm:"primaryKey"`
 	FullName   string `json:"fullName"`
@@ -17,6 +26,31 @@ type ShoppersResponse struct {
 
 type ErrorResponse struct {
 	Error string `json:"error"`
+}
+
+var db *gorm.DB
+
+func initDB() {
+
+	//ต้อง load environment variables จาก .env file ก่อนเสมอ
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("environment variable DATABASE_URL is required")
+	}
+
+	var err error
+	db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("failed to connect to database: %v", err)
+	}
+
+	if err := db.AutoMigrate(&Shopper{}); err != nil {
+		log.Fatalf("failed to migrate database: %v", err)
+	}
 }
 
 func main() {
